3bf44e4852580e5416f90c7aa8d62cf85c600978
YuHoYe <yeyuhao02@corp.netease.com>
2026-03-20 11:18:22 +0800

    Update CCDock to v1.3.0

--- Casks/ccdock.rb
+++ Casks/ccdock.rb
@@ -1,6 +1,6 @@
 cask "ccdock" do
-  version "1.2.0"
-  sha256 "6163711e78320cc842584247c1eb160874c83a0373446882ed7fff0e96da5d8d"
+  version "1.3.0"
+  sha256 "df2bc784469098d36195609405132eb888f9ba1000a57be9adc72e84d83ffbf0"
 
   url "https://github.com/YuHoYe/CCDock/releases/download/v#{version}/CCDock.zip"
   name "CCDock"
